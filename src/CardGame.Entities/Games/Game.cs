2a461a02827f14cbcf92446b622922c35ff36688
drlsn <dariusz.lusnia@gmail.com>
2023-06-27 00:58:08 +0200

    Improve game rules

--- src/CardGame.Entities/Games/Game.cs
+++ src/CardGame.Entities/Games/Game.cs
@@ -7,7 +7,9 @@ public record GameId(string Value) : EntityId(Value);
 
 public class Game : Entity<GameId>
 {
+    public Player[] Players { get; private set; }
     public Deck[] Decks { get; private set; }
+    public Deck CommonPool { get; private set; }
 
     public Game(
         GameId id, 
@@ -16,8 +18,52 @@ public class Game : Entity<GameId>
         Decks = decks;
     }
 
-    public void TakeHalfCardsToCommonPool()
+    public void TakeHalfCardsToCommonPool(object random)
     {
+        var deckRandomHalf = Players.Select(player =>
+        {
+            // player.Deck.Shuffle(random);
+            return default(Deck);// player.Deck.TakeHalf(random);
+        });
 
+        // CommonPool = deckRandomHalf;
+
+        // player.Cards.TakeRandomHalf(random)
+        // player.Cards.TakeRandomHalf(random)
     }
+
+    public void DrawCardsToHand()
+    {
+        var deckRandomHalf = Players.Select(player =>
+        {
+            // player.Deck.Shuffle(random);
+            return default(Deck);// player.Deck.TakeHalf(random);
+        });
+
+        // CommonPool = deckRandomHalf;
+
+        // player.Cards.TakeRandomHalf(random)
+        // player.Cards.TakeRandomHalf(random)
+    }
+
+    public void CalculateRoundPlayerOrder()
+    {
+        var deckRandomHalf = Players.Select(player =>
+        {
+            // player.GetOverallSpeed();
+            return default(Deck);// player.Deck.TakeHalf(random);
+        });
+
+        // CommonPool = deckRandomHalf;
+
+        // player.Cards.TakeRandomHalf(random)
+        // player.Cards.TakeRandomHalf(random)
+    }
+}
+
+public record PlayerId(string Value) : EntityId(Value);
+public class Player : Entity<PlayerId>
+{
+    public Deck Deck { get; private set; }
+    public object HandCards { get; private set; }
 }
