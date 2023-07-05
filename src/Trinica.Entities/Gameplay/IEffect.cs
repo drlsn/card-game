ad047d7308b7b63fa48dcbd5474be7abb68ac6ec
drlsn <dariusz.lusnia@gmail.com>
2023-07-05 12:27:35 +0200

    Add and improve some effects

--- src/Trinica.Entities/Gameplay/IEffect.cs
+++ src/Trinica.Entities/Gameplay/IEffect.cs
@@ -1,4 +1,7 @@
-﻿namespace Trinica.Entities.Gameplay;
+﻿using Corelibs.Basic.Collections;
+using Corelibs.Basic.Functional;
+
+namespace Trinica.Entities.Gameplay;
 
 public interface IEffect
 {
@@ -17,6 +20,16 @@ public class RoundInfo
     public int RoundsLeft => RoundsTotal - RoundsPassed;
 }
 
+public record ReceiveActors(
+    ICombatCard MoveActor,
+    ICombatCard[] AllEnemies,
+    ICombatCard[] AllAllies);
+
+public record MoveActors(
+    ICombatCard[] Targets,
+    ICombatCard[] AllEnemies,
+    ICombatCard[] AllAllies);
+
 public abstract class Effect
 {
     public abstract string Name { get; }
@@ -26,10 +39,12 @@ public abstract class Effect
 
     public virtual void OnEffectStart(ICombatCard effectOwner) {}
     public virtual void OnRoundStart(ICombatCard effectOwner) {}
-    public virtual void BeforeDefend(ICombatCard effectOwner, ICombatCard attacker) {}
-    public virtual void AfterDefend(ICombatCard effectOwner, ICombatCard attacker) { }
-    public virtual void BeforeMove(ICombatCard effectOwner, ICombatCard defender, MoveType moveType) { }
-    public virtual void AfterMove(ICombatCard effectOwner, ICombatCard defender, MoveType moveType) { }
+    public virtual void BeforeReceive(ICombatCard effectOwner, ReceiveActors actors) {}
+    public virtual void AfterReceive(ICombatCard effectOwner, ReceiveActors actors) { }
+    public virtual void BeforeMoveAtSingleTarget(ICombatCard effectOwner, ICombatCard target, MoveType moveType) { }
+    public virtual void AfterMoveAtSingleTarget(ICombatCard effectOwner, ICombatCard target, MoveType moveType) { }
+    public virtual void BeforeMoveAtAll(ICombatCard effectOwner, MoveActors actors, MoveType moveType) { }
+    public virtual void AfterMoveAtAll(ICombatCard effectOwner, MoveActors actors, MoveType moveType) { }
     public virtual void OnRoundFinish(ICombatCard effectOwner) {}
 
     public void RemoveEffects(StatisticPointGroup statistics)
@@ -39,6 +54,12 @@ public abstract class Effect
         statistics.Speed.RemoveAll(Name);
         statistics.Power.RemoveAll(Name);
     }
+
+    public void RemoveEffects(ICombatCard combatCard) =>
+        RemoveEffects(combatCard.Statistics);
+
+    public void RemoveEffects(IEnumerable<ICombatCard> combatCards) =>
+        combatCards.ForEach(RemoveEffects);
 }
 
 public enum MoveType
@@ -82,7 +103,7 @@ public class BleedingEffect : Effect, IEffect
     public bool IsStacking => false;
     public int DamageInflict { get; }
 
-    public override void BeforeMove(ICombatCard effectOwner, ICombatCard defender, MoveType moveType)
+    public override void BeforeMoveAtAll(ICombatCard effectOwner, MoveActors actors, MoveType moveType)
     {
         effectOwner.Statistics.HP.Modify(DamageInflict, Name);
     }
@@ -94,14 +115,32 @@ public class ElectrificationEffect : Effect, IEffect
     public bool IsStacking => false;
     public int DamageInflict { get; }
 
-    public override void BeforeMove(ICombatCard effectOwner, ICombatCard defender, MoveType moveType)
+    public override void BeforeMoveAtAll(ICombatCard effectOwner, MoveActors actors, MoveType moveType)
     {
         if (moveType is not MoveType.Skill)
             return;
-
+        
         effectOwner.Statistics.HP.Modify(DamageInflict, Name);
         if (RoundInfo.RoundsPassed == 1)
             effectOwner.Statistics.Attack.Modify(0.5, Name, isFactor: true);
+    }
+}
+
+public class ShieldEffect : Effect, IEffect
+{
+    public override string Name => "Shield";
+    public bool IsStacking => true;
+
+    public int DamageAbsorption { get; } = 300;
+
+    public override void BeforeReceive(ICombatCard effectOwner, ReceiveActors actors)
+    {
+        actors.MoveActor.Statistics.Attack.Modify(-DamageAbsorption, Name);
+    }
+
+    public override void AfterReceive(ICombatCard effectOwner, ReceiveActors actors)
+    {
+        RemoveEffects(actors.MoveActor.Statistics);
     }
 }
 
@@ -111,37 +150,53 @@ public class PiercingDamageEffect : Effect, IEffect
     public bool IsStacking => false;
     public int DamageInflict { get; }
 
-    public override void BeforeMove(ICombatCard effectOwner, ICombatCard defender, MoveType moveType)
+    private ShieldEffect[] _targetShieldEffects;
+
+    public override void BeforeMoveAtAll(ICombatCard effectOwner, MoveActors actors, MoveType moveType)
     {
         if (moveType is not MoveType.Attack)
             return;
 
-        //defender.Statistics
+        _targetShieldEffects = actors.Targets
+            .Select(t => t.Effects)
+            .Select(effects => effects.OfType<ShieldEffect>())
+            .Aggregate((x, y) => x.Concat(y))
+            .ToArray();
+
+        _targetShieldEffects.ForEach(e => e.Enabled = false);
     }
 
-    public override void AfterMove(ICombatCard effectOwner, ICombatCard defender, MoveType moveType)
+    public override void AfterMoveAtAll(ICombatCard effectOwner, MoveActors actors, MoveType moveType)
     {
         if (moveType is not MoveType.Attack)
             return;
 
-        //defender.Statistics
+        _targetShieldEffects.ForEach(e => e.Enabled = true);
+        _targetShieldEffects = null;
     }
 }
 
-public class ShieldEffect : Effect, IEffect
+public class CrushingDamageEffect : Effect, IEffect
 {
-    public override string Name => "Shield";
-    public bool IsStacking => true;
-    public int DamageInflict { get; }
+    public override string Name => "Crushing Damage";
+    public bool IsStacking => false;
+    public int DamageFactor { get; } = 2;
 
-    public override void BeforeDefend(ICombatCard effectOwner, ICombatCard attacker)
+    public override void BeforeMoveAtSingleTarget(ICombatCard effectOwner, ICombatCard target, MoveType moveType)
     {
-        attacker.Statistics.Attack.ModifyLate(0, Name, isFactor: true);
+        if (moveType is not MoveType.Attack)
+            return;
+
+        if (target.Effects.OfType<ShieldEffect>().Count() > 0)
+            effectOwner.Statistics.Attack.Modify(DamageFactor, Name, isFactor: true);
     }
 
-    public override void AfterDefend(ICombatCard effectOwner, ICombatCard attacker)
+    public override void AfterMoveAtSingleTarget(ICombatCard effectOwner, ICombatCard target, MoveType moveType)
     {
-        RemoveEffects(attacker.Statistics);
+        if (moveType is not MoveType.Attack)
+            return;
+
+        RemoveEffects(target);
     }
 }
 
@@ -150,9 +205,14 @@ public class CriticEffect : Effect, IEffect
     public override string Name => "Critical Damage";
     public bool IsStacking => false;
 
-    public override void BeforeMove(ICombatCard effectOwner, ICombatCard defender, MoveType moveType)
+    public override void BeforeMoveAtAll(ICombatCard effectOwner, MoveActors actors, MoveType moveType)
     {
         effectOwner.Statistics.Attack.Modify(2, Name, isFactor: true);
+    }
+
+    public override void AfterMoveAtAll(ICombatCard effectOwner, MoveActors actors, MoveType moveType)
+    {
+        RemoveEffects(effectOwner);
     }
 }
 
